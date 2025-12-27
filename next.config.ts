9aa3b161673c5c59bce17d4cf7a884d8b4cbe10d
obregon-jose <jo46646@gmail.com>
2025-12-26 21:45:52 -0500

    corrección imagenes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import { SITE_DOMAIN } from "@/lib/seo-config";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    formats: ['image/avif', 'image/webp', 'image/png', 'image/gif'],
+    domains: [SITE_DOMAIN],
+  }
 };
 
 export default nextConfig;
